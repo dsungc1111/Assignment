031adf3889b75b1afd31df8df75a1e088bfd7e88
dsungc1111 <eotjd9808@naver.com>
2024-05-20 15:57:10 +0900

    Initial Commit

---
+++ Assignment0520/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment0520
+//
+//  Created by 최대성 on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
