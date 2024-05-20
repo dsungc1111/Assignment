679ae9b6a62975207c5295df40390b217415904a
dsungc1111 <eotjd9808@naver.com>
2024-05-20 19:25:24 +0900

    Home 화면 수정

--- Assignment0520/ViewControllerGroup/HomeViewController.swift
+++ Assignment0520/ViewControllerGroup/HomeViewController.swift
@@ -9,10 +9,62 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet var idTextField: UITextField!
+    
+    @IBOutlet var nickNameTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    @IBOutlet var signInButton: UIButton!
+    
+    @IBOutlet var infoTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .black
+        
+        
+        loginWindow(idTextField, loginWindowContent: "id 혹은 e-mail을(를) 입력하세요")
+        loginWindow(nickNameTextField, loginWindowContent: "닉네임을 입력하세요.")
+        loginWindow(passwordTextField, loginWindowContent: "비밀번호를 입력하세요.")
+        loginWindow(infoTextField, loginWindowContent: "추가 정보 입력")
+        signInButtonShape()
     }
+    
+    func loginWindow(_ box: UITextField, loginWindowContent text: String) {
+        box.becomeFirstResponder()
+        box.layer.borderWidth = 1
+        box.backgroundColor = .gray
+        box.text = text
+        box.textColor = .white
+        box.textAlignment = .center
+        box.clearButtonMode = .always
+        if box == infoTextField {
+            box.textAlignment = .left
+            box.textColor = .white
+            box.backgroundColor = .black
+        }
+    }
+    
+    func signInButtonShape() {
+        signInButton.layer.borderColor = UIColor.white.cgColor
+        signInButton.layer.borderWidth = 1
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
     
 
 }
