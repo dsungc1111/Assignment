531bd1e803d8fe9d2f0997a86d432b1fd0084b7b
dsungc1111 <eotjd9808@naver.com>
2024-05-26 22:24:20 +0900

    - 식당 조회 > 카테고리별로 검색 가능 - 즐겨찾기 선택 가능 - 셀 안에 이미지, 가게이름, 번호, 최소주문가격, 주소 기입

---
+++ SeSAC0524Assignment/SeSAC0524Assignment/restaurantGroup/RestaurantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantTableViewCell.swift
+//  SeSAC0524Assignment
+//
+//  Created by 최대성 on 5/26/24.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var restaurantNameLabel: UILabel!
+    
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var minimumPriceLabel: UILabel!
+    
+    @IBOutlet var adressLabel: UILabel!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+}
